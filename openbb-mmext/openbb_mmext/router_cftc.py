e0687ecf4bb1f045621ead7fed7068388f31b452
mmistroni <mmistroni@gmail.com>
2024-05-03 12:53:34 +0100

    another experimentn iwth anotehr router

---
+++ openbb-mmext/openbb_mmext/router_cftc.py
@@ -0,0 +1,26 @@
+"""OpenBB mmext router command example."""
+
+import requests
+from openbb_core.app.model.command_context import CommandContext
+from openbb_core.app.model.obbject import OBBject
+from openbb_core.app.provider_interface import (ExtraParams, ProviderChoices,
+                                                StandardParams)
+from openbb_core.app.query import Query
+from openbb_core.app.router import Router
+from pydantic import BaseModel
+from openbb_mmext.utils.helpers import get_nymo, get_nysi
+
+router = Router(prefix="")
+
+
+@router.command(model="CommitmentOfTradersc")
+async def get_cftc_cot(
+    cc: CommandContext,
+    provider_choices: ProviderChoices,
+    standard_params: StandardParams,
+    extra_params: ExtraParams,
+) -> OBBject[BaseModel]:
+    """Example Data."""
+    return await OBBject.from_query(Query(**locals()))
+
+
