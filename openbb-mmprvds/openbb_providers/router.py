45fd42e0819f043bf66ba079248b7132ce0698cf
mmistroni <mmistroni@gmail.com>
2024-05-12 10:23:31 +0100

    committing first iteration

--- openbb-mmprvds/openbb_providers/router.py
+++ openbb-mmprvds/openbb_providers/router.py
@@ -1,5 +1,3 @@
-"""OpenBB mmext router command example."""
-
 import requests
 from openbb_core.app.model.command_context import CommandContext
 from openbb_core.app.model.obbject import OBBject
@@ -13,8 +11,12 @@ from openbb_mmext.utils.helpers import get_nymo, get_nysi
 router = Router(prefix="")
 
 
-@router.command(model="CommitmentOfTradersc")
-async def get_cftc_cot(
+@router.command(methods=["GET"])
+def xot() -> OBBject[dict]:
+    return OBBject(results = {'foo' : 'bar'})
+
+@router.command(model="CommitmentOfTradersList")
+async def cot_list(
     cc: CommandContext,
     provider_choices: ProviderChoices,
     standard_params: StandardParams,
@@ -23,4 +25,12 @@ async def get_cftc_cot(
     """Example Data."""
     return await OBBject.from_query(Query(**locals()))
 
-
+@router.command(model="CommitmentOfTradersAnalysis")
+async def cot(
+    cc: CommandContext,
+    provider_choices: ProviderChoices,
+    standard_params: StandardParams,
+    extra_params: ExtraParams,
+) -> OBBject[BaseModel]:
+    """Example Data."""
+    return await OBBject.from_query(Query(**locals()))
