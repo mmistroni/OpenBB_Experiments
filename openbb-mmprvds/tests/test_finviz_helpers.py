ee82015e742033248ecaf18dc03835f00865b638
mmistroni <mmistroni@gmail.com>
2024-07-04 06:10:50 +0100

    resurrected finviz screener

---
+++ openbb-mmprvds/tests/test_finviz_helpers.py
@@ -0,0 +1,25 @@
+import pytest
+
+from openbb_providers.utils.finviz_helpers import run_screener
+
+def test_sample_screener():
+    generic_filter_dict = {'Market Cap.': '+Small (over $300mln)',
+                           'Average Volume': 'Over 200K',
+                           'Price': 'Over $10',
+                           'EPS growththis year': 'Over 20%'}
+
+    data = run_screener(generic_filter_dict)
+    assert bool(data)
+
+
+
+def test_get_sample_screener_with_exception():
+    generic_filter_dict = {'Market Cap.': 'all (over $300mln)',
+                           'Average Volume': 'Over 200K'}
+    with pytest.raises(Exception) as exc_info:
+        data = run_screener(generic_filter_dict)
+    # these asserts are identical; you can use either one
+    assert 'Invalid filter' in exc_info.value.args[0]
+
+
+
