2382d334e99b92b41fe3bfa906e261334b6fc73c
mmistroni <mmistroni@gmail.com>
2025-01-15 21:12:55 +0000

    adding adamodaran data

---
+++ openbb-mmprvds/openbb_providers/models/damod_provider.py
@@ -0,0 +1,83 @@
+from typing import Any, Dict, List, Optional
+from datetime import date
+from openbb_core.provider.abstract.data import Data
+from openbb_core.provider.abstract.fetcher import Fetcher
+from openbb_core.provider.abstract.query_params import QueryParams
+from pydantic import Field
+from openbb_providers.utils.finviz_helpers import run_screener
+from pydantic import BaseModel, model_validator
+from typing import Optional, List
+from utils.damod_utils import get_betas, get_fgrowtheps, get_pe,\
+                              get_ps, get_pvdata, get_roe
+import json
+
+import logging
+
+import requests
+
+
+class RoeQueryParams(QueryParams):
+    """
+    Query Parameters for ADamodaran finviz screener
+
+    https://pypi.org/project/finvizfinance/
+
+    """
+
+class RoeData(Data):
+    """Sample provider data for finviz screener.
+		
+    """
+    industry_name: str = Field(description="Industry Name", alias='Industry Name')
+    number_of_firms: int = Field(description="Number of Firms.", alias='Number of firms')
+    roe_unadjusted: str = Field(description="ROE unadjusted", alias='ROE (adjusted for R&D)')
+    roe_adjusted_rd : str  = Field(description="ROE adjusted for r & d.", alias="Industry")
+    
+
+
+class RoeFetcher(
+    Fetcher[
+        RoeQueryParams,
+        List[RoeData],
+    ]
+):
+    """ Finviz Screener Fetcher class.
+
+    This class is responsible for the actual data retrieval.
+    """
+
+
+    @staticmethod
+    def transform_query(params: Dict[str, Any]) -> RoeQueryParams:
+        """Define example transform_query.
+
+        Here we can pre-process the query parameters and add any extra parameters that
+        will be used inside the extract_data method.
+        """
+        return RoeQueryParams(**params)
+
+    @staticmethod
+    def extract_data(
+        query: RoeQueryParams,
+        credentials: Optional[Dict[str, str]],
+        **kwargs: Any,
+    ) -> List[dict]:
+        """Define example extract_data.
+
+        Here we make the actual request to the data provider and receive the raw data.
+        If you said your Provider class needs credentials you can get them here.
+        """
+        return get_roe()
+
+    @staticmethod
+    def transform_data(
+        query: RoeQueryParams, data: List[dict], **kwargs: Any
+    ) -> List[RoeData]:
+        """Define example transform_data.
+
+        Right now, we're converting the data to fit our desired format.
+        You can apply other transformations to it here.
+        """
+        return [RoeData(**d) for d in data]
+
+
--- openbb-mmprvds/openbb_providers/utils/damod_utils.py
+++ openbb-mmprvds/openbb_providers/utils/damod_utils.py
@@ -2,9 +2,65 @@ from bs4 import BeautifulSoup
 import requests
 from datetime import date, datetime
 import random
+from io import BytesIO
+import pandas as pd
+import logging
 
-def get_industry_multiples(year=None):
-    baseUrl = 'https://pages.stern.nyu.edu/~adamodar/New_Home_Page/datafile/psdata.html'
-    histUrl = 'https://pages.stern.nyu.edu/~adamodar/New_Home_Page/dataarchived.html#multiples'
+psRatiosUrlTTM = 'https://www.stern.nyu.edu/~adamodar/pc/datasets/psdata.xls'
+betasBySectorTTM = 'https://www.stern.nyu.edu/~adamodar/pc/datasets/betas.xls'
+fundgrowepsTTM = 'https://www.stern.nyu.edu/~adamodar/pc/datasets/fundgr.xls'
+roeTTM = 'https://www.stern.nyu.edu/~adamodar/pc/datasets/roe.xls'
+peRatiosTTM = 'https://www.stern.nyu.edu/~adamodar/pc/datasets/pedata.xls'
+pvDataTTM = 'https://www.stern.nyu.edu/~adamodar/pc/datasets/pbvdata.xls'
 
+def _get_data(url):
+    try:
+        response = requests.get(url, verify=False)
+        response.raise_for_status()
+        xls_file = BytesIO(response.content)
+        return pd.read_excel(xls_file).to_dict('records')
+    except Exception as e:
+        logging.info(f'Failed to fetch {url}:{str(e)}')
+        raise Exception(f'Failed to fetch {url}:{str(e)}')
 
+def get_roe():
+    '''
+    Return on Equity by Sector (US)
+    https://pages.stern.nyu.edu/~adamodar/New_Home_Page/datafile/roe.html
+    '''
+    return _get_data(roeTTM)
+    
+def get_ps():
+    '''
+    Revenue Multiples by Sector (US)
+    https://pages.stern.nyu.edu/~adamodar/New_Home_Page/datafile/psdata.html
+    '''
+    return _get_data(psRatiosUrlTTM)
+
+def get_fgrowtheps():
+    '''
+    Fundamental Growth in EPS by Sector (US)
+    https://pages.stern.nyu.edu/~adamodar/New_Home_Page/datafile/fundgr.html
+    '''
+    return _get_data(fundgrowepsTTM)
+
+def get_pe():
+    '''
+    PE Ratio by Sector (US)
+    https://pages.stern.nyu.edu/~adamodar/New_Home_Page/datafile/pedata.html
+    '''
+    return _get_data((peRatiosTTM))
+
+def get_betas():
+    '''
+    Betas by Sector (US)
+    https://pages.stern.nyu.edu/~adamodar/New_Home_Page/datafile/Betas.html
+    '''
+    return _get_data(betasBySectorTTM)
+
+def get_pvdata():
+    '''
+    Price and Value to Book Ratio by Sector (US)
+    https://pages.stern.nyu.edu/~adamodar/New_Home_Page/datafile/pbvdata.html
+    '''
+    return _get_data(pvDataTTM)
