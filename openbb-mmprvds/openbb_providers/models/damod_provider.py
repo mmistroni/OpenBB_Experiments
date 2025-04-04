bac1fe98f0cf5dd98b98af5d845ed6c05e5bdc1e
mmistroni <mmistroni@gmail.com>
2025-04-04 17:18:37 +0100

    committing working code with finviz screener

--- openbb-mmprvds/openbb_providers/models/damod_provider.py
+++ openbb-mmprvds/openbb_providers/models/damod_provider.py
@@ -8,7 +8,7 @@ from openbb_providers.utils.finviz_helpers import run_screener
 from pydantic import BaseModel, model_validator
 from typing import Optional, List
 from openbb_providers.utils.damod_utils import get_betas, get_fgrowtheps, get_pe,\
-                              get_ps, get_pvdata, get_roe
+                              get_ps, get_pvdata, get_roe, get_valuation_metrics
 import json
 
 import logging
@@ -79,5 +79,83 @@ class RoeFetcher(
         You can apply other transformations to it here.
         """
         return [RoeData(**d) for d in data]
+
+
+class IndustryValuationQueryParams(QueryParams):
+    """
+    Query Parameters for ADamodaran data
+
+    """
+
+
+class IndustryValuationData(Data):
+    """Sample provider damod roe data.
+
+    """
+    industry_name: str = Field(description="Industry Name", alias='Industry Name')
+    number_of_firms: int = Field(description="Number of Firms.", alias='Number of firms')
+    roe: float = Field(description="ROE unadjusted", alias='ROE')
+    fundamental_growth: float = Field(description="Industry Fundamental Growth", alias='Fundamental Growth')
+    current_pe: float = Field(description="Industry Current PE", alias='Current PE')
+    trailing_pe: float = Field(description="Industry Fundamental Growth", alias='Trailing PE')
+    forward_pe: float = Field(description="Industry Fundamental Growth", alias='Forward PE')
+    expected_growth_5y: float = Field(description="Industry Fundamental Growth", alias='Expected Growth - next 5 years')
+    peg: float = Field(description="Industry Fundamental Growth", alias='PEG Ratio')
+    pbv: float = Field(description="Industry Fundamental Growth", alias='PBV',)
+    ev_invested_capital: float = Field(description="Industry Fundamental Growth", alias='EV/Invested Capital')
+    roic: float = Field(description="Industry Fundamental Growth", alias='ROIC')
+    ceo_holdings: float = Field(description="Industry Fundamental Growth", alias='CEO Holdings',)
+    corporate_holdings: float = Field(description="Industry Fundamental Growth", alias='Corporate Holdings')
+    institutional_holdings: float = Field(description="Industry Fundamental Growth", alias='Insitutional Holdings')
+    insider_holdings: float = Field(description="Industry Fundamental Growth", alias='Insider Holdings')
+
+class IndustryValuationFetcher(
+    Fetcher[
+        IndustryValuationQueryParams,
+        List[IndustryValuationData],
+    ]
+):
+    """ Finviz Screener Fetcher class.
+
+    This class is responsible for the actual data retrieval.
+    """
+
+    @staticmethod
+    def transform_query(params: Dict[str, Any]) -> IndustryValuationQueryParams:
+        """Define example transform_query.
+
+        Here we can pre-process the query parameters and add any extra parameters that
+        will be used inside the extract_data method.
+        """
+        return IndustryValuationQueryParams(**params)
+
+    @staticmethod
+    def extract_data(
+            query: IndustryValuationQueryParams,
+            credentials: Optional[Dict[str, str]],
+            **kwargs: Any,
+    ) -> List[dict]:
+        """Define example extract_data.
+
+        Here we make the actual request to the data provider and receive the raw data.
+        If you said your Provider class needs credentials you can get them here.
+        """
+        metrics_df = get_valuation_metrics()
+        return metrics_df.to_dict('records')
+
+    @staticmethod
+    def transform_data(
+            query: IndustryValuationQueryParams, data: List[dict], **kwargs: Any
+    ) -> List[RoeData]:
+        """Define example transform_data.
+
+        Right now, we're converting the data to fit our desired format.
+        You can apply other transformations to it here.
+        """
+        return [IndustryValuationData(**d) for d in data]
+
+
+
+
 
 
--- pyth2/README.md
+++ pyth2/README.md
@@ -17,52 +17,7 @@ Documentation available [here](https://docs.openbb.co/sdk).
 https://bvb.ro/TradingAndStatistics/Trading/MiFID_II_PostTradeListForDownload.ashx?filetype=xlsx
 
 
-pyproject.toml we define the extension
+pyproject.toml
+defines pyth2 = "openbb_pyth2:pyth2_provider
 
-```pyth2 = "openbb_pyth2:pyth2_provider"```
-
-This provider is then defined in  openbb_pyth2\__init__.py
-
-The provider would refer to a fetcher which is defined in openbb_pyth2.models.__init__.py
-
-#### Steps to get there
-
-1. Create an anaconda env
-2. Use this env to get openbb from git    git clone https://github.com/OpenBB-finance/OpenBBTerminal.git
-3. Build openbb following instructions here https://docs.openbb.co/platform/installation#source
-4. Test the build 
-        from openbb import obb
-        obb.account.login(email='mmistroni@gmail.com', password='**')
-        obb.user.preferences.output_type = "dataframe"
-        obb.equity.price.historical(symbol = 'XOM', provider='fmp')
-5. Build your extension
-6. Run a simple unit test to verify you can import libraries from OBB
-
-Cloned GitHub repo
-How did you "install" OpenBB after this?
-It works.
-cd.. into your extension directory, and install pip install -e .
-Then, from openbb import obb fails?
-
-
-
-(openbb_latest) C:\Users\Marco And Sofia\OpenBB\OpenBB_Experiments\pyth2>python
-Python 3.11.8 | packaged by Anaconda, Inc. | (main, Feb 26 2024, 21:34:05) [MSC v.1916 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license" for more information.
->>> from openbb import obb
-Extensions to add: pyth2@1.0.0
-
-Building...
-
-
-
-(openbb_latest) C:\Users\Marco And Sofia\OpenBB\OpenBB_Experiments\pyth2>python
-Python 3.11.8 | packaged by Anaconda, Inc. | (main, Feb 26 2024, 21:34:05) [MSC v.1916 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license" for more information.
->>> from openbb import obb
-Extensions to add: pyth2@1.0.0
-
-Building...
-
-To launch the API you can run: uvicorn openbb_core.api.rest_api:app --host 0.0.0.0 --port 8000 --reload
-You can specify the host and port parameters on that command - this might also help:
+this provider is in turn defined in the oopenbb_pyth2.__init__.py
